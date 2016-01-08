dd41fa88ea09a5120296cfd139006bd40f51f840
Michael Wehrle <mwehrle@scrippsnetworks.com>
2016-01-07 22:12:39 -0500

    function for creating a duration, which is a portion of a run with specific duration, distance and grade

--- gpxGen.py
+++ gpxGen.py
@@ -4,7 +4,6 @@ from datetime import timedelta
 
 default_lon = '-84.0704070'
 default_startTime = datetime.datetime.now() + timedelta(hours=-4)
-#a = a + timedelta(seconds=1) //to be used to add a second to existing datetime var
 
 def create_trkpt ( trkseg, lat, lon, ele, time ):
     trkpt = ET.SubElement(trkseg, "trkpt")
@@ -14,6 +13,16 @@ def create_trkpt ( trkseg, lat, lon, ele, time ):
     trkpt_ele.text = ele
     trkpt_time = ET.SubElement(trkpt, 'time')
     trkpt_time.text = time
+    return
+
+def create_duration ( startTime, startLat, startLon, seconds, miles, grade ):
+    currentTime = startTime
+    for second in range(seconds):
+        currentTime = currentTime + timedelta(seconds=1)
+        #format the date to what garmin expects '2016-01-07T15:00:00Z'
+        #calculate each lon iteration based on miles / seconds
+        #calculate elevation based on grade
+        print currentTime
     return
 
 root = ET.Element('gpx')
@@ -29,12 +38,13 @@ time.text = '2016-01-07T15:00:00Z'
 trk = ET.SubElement(root, 'trk')
 trkseg = ET.SubElement(trk, 'trkseg')
 
-create_trkpt(trkseg, '35.0000000', default_lon, '0.0', '2016-01-07T15:00:00Z')
-create_trkpt(trkseg, '35.0145000', default_lon, '0.0', '2016-01-07T15:08:00Z')
-create_trkpt(trkseg, '35.0290000', default_lon, '80.4672', '2016-01-07T15:17:00Z')
-create_trkpt(trkseg, '35.0435000', default_lon, '160.9344', '2016-01-07T15:26:00Z')
-create_trkpt(trkseg, '35.0580000', default_lon, '241.4016', '2016-01-07T15:35:00Z')
-create_trkpt(trkseg, '35.0725000', default_lon, '321.8688', '2016-01-07T15:44:00Z')
+create_duration(default_startTime, '35.0000000', default_lon, 60, 1, 0)
+#create_trkpt(trkseg, '35.0000000', default_lon, '0.0', '2016-01-07T15:00:00Z')
+#create_trkpt(trkseg, '35.0145000', default_lon, '0.0', '2016-01-07T15:08:00Z')
+#create_trkpt(trkseg, '35.0290000', default_lon, '80.4672', '2016-01-07T15:17:00Z')
+#create_trkpt(trkseg, '35.0435000', default_lon, '160.9344', '2016-01-07T15:26:00Z')
+#create_trkpt(trkseg, '35.0580000', default_lon, '241.4016', '2016-01-07T15:35:00Z')
+#create_trkpt(trkseg, '35.0725000', default_lon, '321.8688', '2016-01-07T15:44:00Z')
 
 tree = ET.ElementTree(root)
 ET.dump(root)
